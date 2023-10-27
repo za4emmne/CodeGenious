d53f1b6cdef793a75b5a74602da1a3bf1ddaf506
za4emmne <iluhaha@mail.ru>
2023-10-27 21:23:14 +0500

    first

---
+++ GoPlaces.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoPlaces : MonoBehaviour
+{
+    private float _float;
+    private Transform _allPlacespoint;
+    private Transform[] _arrayPlaces;
+    private int _numberOfPlaceInArrayPlaces;
+
+    void Start()
+    {
+        _arrayPlaces = new Transform[_allPlacespoint.childCount];
+
+        for (int i = 0; i < _allPlacespoint.childCount; i++)
+        {
+            _arrayPlaces[i] = _allPlacespoint.GetChild(i).GetComponent<Transform>();
+        }
+    }
+
+    public void Update()
+    {
+        var pointByNumberInArray = _arrayPlaces[_numberOfPlaceInArrayPlaces];
+        transform.position = Vector3.MoveTowards(transform.position, pointByNumberInArray.position, _float * Time.deltaTime);
+
+        if (transform.position == pointByNumberInArray.position)
+        {
+            NextPlaceTakerLogic();
+        }
+    }
+
+    public Vector3 NextPlaceTakerLogic()
+    {
+        _numberOfPlaceInArrayPlaces++;
+
+        if (_numberOfPlaceInArrayPlaces == _arrayPlaces.Length)
+            _numberOfPlaceInArrayPlaces = 0;
+
+        var thisPointVector = _arrayPlaces[_numberOfPlaceInArrayPlaces].transform.position;
+        transform.forward = thisPointVector - transform.position;
+        return thisPointVector;
+    }
+}
---
+++ InstantiateBulletsShooting.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class InstantiateBulletsShooting : MonoBehaviour
+{
+    [SerializeField] private float _number;
+    [SerializeField] private GameObject _prefab;
+    [SerializeField] private float _timeWaitShooting;
+    private Transform _objectToShoot;
+
+    void Start()
+    {
+        StartCoroutine(ShootingWorker());
+    }
+
+    private IEnumerator ShootingWorker()
+    {
+        bool isWork = enabled;
+
+        while (isWork)
+        {
+            var _vector3direction = (ObjectToShoot.position - transform.position).normalized;
+            var NewBullet = Instantiate(_prefab, transform.position + _vector3direction, Quaternion.identity);
+            NewBullet.GetComponent<Rigidbody>().transform.up = _vector3direction;
+            NewBullet.GetComponent<Rigidbody>().velocity = _vector3direction * _number;
+            yield return new WaitForSeconds(_timeWaitShooting);
+        }
+    }
+}
